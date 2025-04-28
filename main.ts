71ffc80b74d9fcab207356b2ede14a8ed969a8c5
OscarJimenez <62412378+Marzopa@users.noreply.github.com>
2025-04-28 14:58:38 -0400

    made the example graph

--- main.ts
+++ main.ts
@@ -1,18 +1,63 @@
 function initializeGraph () {
-    list = [0, 1, 1]
-    list2 = [0, 0, 0]
-    final = [list, list2]
+    row1 = [
+    1,
+    9,
+    1,
+    1,
+    1,
+    1,
+    1
+    ]
+    row2 = [
+    1,
+    9,
+    1,
+    9,
+    1,
+    1,
+    1
+    ]
+    row3 = [
+    1,
+    9,
+    1,
+    9,
+    9,
+    9,
+    1
+    ]
+    row4 = [
+    1,
+    1,
+    1,
+    9,
+    2,
+    1,
+    2
+    ]
+    row5 = [
+    5,
+    7,
+    6,
+    8,
+    3,
+    7,
+    4
+    ]
+    final = [
+    row1,
+    row2,
+    row3,
+    row4,
+    row5
+    ]
     graph = Dijkstra.gridToGraph(final)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
 }
 let graph: Dijkstra.Graph = null
 let final: number[][] = []
-let list2: number[] = []
-let list: number[] = []
+let row5: number[] = []
+let row4: number[] = []
+let row3: number[] = []
+let row2: number[] = []
+let row1: number[] = []
 initializeGraph()
