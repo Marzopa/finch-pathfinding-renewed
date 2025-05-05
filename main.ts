78425f0cc66413d7930f5da49b0138d7cfac9551
OscarJimenez <62412378+Marzopa@users.noreply.github.com>
2025-05-05 18:58:08 -0400

    before breaking

--- main.ts
+++ main.ts
@@ -53,46 +53,52 @@ function initializeGraph () {
     ]
     graph = Dijkstra.gridToGraph(final)
 }
+function moveFinch () {
+    orientation = 0
+    position = Dijkstra.createPosition(0, 0)
+    basic.showIcon(IconNames.House)
+    path = Dijkstra.dijkstra(graph, position, Dijkstra.createPosition(4, 5))
+    basic.showIcon(IconNames.EighthNote)
+    finch.startFinch()
+    for (let step of path) {
+        if (Dijkstra.getPositionRow(step) > Dijkstra.getPositionRow(position)) {
+            while (orientation != 2) {
+                finch.setTurn(RLDir.Right, 90, 100)
+                orientation = (orientation + 1) % 4
+            }
+        } else if (Dijkstra.getPositionRow(step) < Dijkstra.getPositionRow(position)) {
+            while (orientation != 0) {
+                finch.setTurn(RLDir.Right, 90, 75)
+                orientation = (orientation + 1) % 4
+            }
+        } else if (Dijkstra.getPositionColumn(step) > Dijkstra.getPositionColumn(position)) {
+            while (orientation != 1) {
+                finch.setTurn(RLDir.Right, 90, 75)
+                orientation = (orientation + 1) % 4
+            }
+        } else if (Dijkstra.getPositionColumn(step) < Dijkstra.getPositionColumn(position)) {
+            while (orientation != 3) {
+                finch.setTurn(RLDir.Right, 90, 75)
+                orientation = (orientation + 1) % 4
+            }
+        } else {
+            basic.showIcon(IconNames.SmallDiamond)
+        }
+        finch.setMove(MoveDir.Forward, 15, 75)
+        position = step
+    }
+}
+let path: Dijkstra.Position[] = []
+let position: Dijkstra.Position = null
+let orientation = 0
+let graph: Dijkstra.Graph = null
 let final: number[][] = []
 let row5: number[] = []
 let row4: number[] = []
 let row3: number[] = []
 let row2: number[] = []
 let row1: number[] = []
-let graph: Dijkstra.Graph = null
 basic.showIcon(IconNames.Heart)
 initializeGraph()
 basic.showIcon(IconNames.Duck)
-let orientation = 0
-let position = Dijkstra.createPosition(0, 0)
-basic.showIcon(IconNames.House)
-let path = Dijkstra.dijkstra(graph, position, Dijkstra.createPosition(4, 5))
-basic.showIcon(IconNames.EighthNote)
-finch.startFinch()
-for (let step of path) {
-    if (Dijkstra.getPositionRow(step) > Dijkstra.getPositionRow(position)) {
-        while (orientation != 2) {
-            finch.setTurn(RLDir.Right, 90, 100)
-            orientation = (orientation + 1) % 4
-        }
-    } else if (Dijkstra.getPositionRow(step) < Dijkstra.getPositionRow(position)) {
-        while (orientation != 0) {
-            finch.setTurn(RLDir.Right, 90, 100)
-            orientation = (orientation + 1) % 4
-        }
-    } else if (Dijkstra.getPositionColumn(step) > Dijkstra.getPositionColumn(position)) {
-        while (orientation != 1) {
-            finch.setTurn(RLDir.Right, 90, 100)
-            orientation = (orientation + 1) % 4
-        }
-    } else if (Dijkstra.getPositionColumn(step) < Dijkstra.getPositionColumn(position)) {
-        while (orientation != 3) {
-            finch.setTurn(RLDir.Right, 90, 100)
-            orientation = (orientation + 1) % 4
-        }
-    } else {
-        basic.showIcon(IconNames.SmallDiamond)
-    }
-    finch.setMove(MoveDir.Forward, 15, 100)
-    position = step
-}
+moveFinch()
